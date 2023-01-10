eca9d435b26b09eb81d7e3da1b07f4bd2d64941c
acf380 <acf38000@gmail.com>
2023-01-10 13:46:18 +0100

    wow

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,16 +12,58 @@ function loadTab(tab_number)
 	last_tab = tab_number;
 }
 
-function editRow(ev, i)
+
+function postData(data) 
 {
-	i = parseInt(i);
-	//var table = document.getElementById('tab1').getElementsByTagName('tbody')[0].getElementsByTagName('tr')[i].getElementsByTagName('td')[4].getElementsByTagName('label')[0].innerText = "chuj";
-	
 	let xml = new XMLHttpRequest();
     xml.open("POST", "/host", true);
 	xml.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xml.send("dupa");
-	
-	console.log(i);
-	
+    xml.send("json="+data);
+} 
+
+
+function editRow(n)
+{	
+	var buttonName = document.getElementById(n).innerText;
+	n = parseInt(n);
+	var len = document.getElementById('tab1').getElementsByTagName('tbody')[0].getElementsByTagName('tr')[n].childElementCount-2;
+
+	switch(buttonName) {
+		case 'Edit!' :
+			for (var i=1; i<len; i++) 
+			{
+				var label = document.getElementById('tab1').getElementsByTagName('tbody')[0].getElementsByTagName('tr')[n].getElementsByTagName('td')[i].getElementsByTagName('label')[0];
+				var input = document.createElement("input");
+				input.setAttribute('size', '5');
+				input.value = label.innerText;
+				label.replaceWith(input);
+			}
+			document.getElementById(n).innerText = "Save!";
+			break;
+
+		case 'Save!' : 
+			var jsonString = '{"sql_data":[';
+			var check = false;
+			for (var i=1; i<len; i++) 
+			{
+				var edit = document.getElementById('tab1').getElementsByTagName('tbody')[0].getElementsByTagName('tr')[n].getElementsByTagName('td')[i].getElementsByTagName('input')[0];
+				var label = document.createElement("label");
+				label.setAttribute('size', '5');
+				if (edit.value != "") 
+				{
+					jsonString += `{"value${i}":"${edit.value}"},`;
+					check = true;
+				}
+				label.innerText = edit.value;
+				edit.replaceWith(label);
+			}
+			if (check == true) jsonString = jsonString.slice(0, -1);
+			jsonString += ']}';
+			postData(jsonString);
+
+			document.getElementById(n).innerText = "Edit!";
+			break;
+		default:
+			console.log("ERROR: WRONG BUTTON NAME!");
+	}
 }
--- routes/host.js
+++ routes/host.js
@@ -5,7 +5,10 @@ var connection  = require('../public/javascripts/database.js');
  
 /* GET home page. */
 router.post('/', function(req, res, next){
-  console.log(req.body);      
+  data = JSON.parse(req.body['json']);
+  console.log(data);
+  console.log(data["sql_data"]);
+  console.log(data["sql_data"][0]["value1"]);
 });
  
  
