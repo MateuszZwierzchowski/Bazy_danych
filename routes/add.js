8879308bb94f2a42b3ed1a984bcb1489f50df364
acf380 <acf38000@gmail.com>
2023-01-15 22:23:46 +0100

    normalnie kox

--- routes/add.js
+++ routes/add.js
@@ -4,15 +4,13 @@ var connection  = require('../public/javascripts/database.js');
  
  
 router.post('/', function(req, res, next) {
-    console.log(req.body);
     data = JSON.parse(JSON.stringify(req.body));
     key = Object.keys(data)[0]; 
 
     switch(key)
     {
-        case 'magazyn':
-            var cellsToAdd = [
-                {"KATRGORIA": false},
+        case 'produkt':
+            var productFeatures = [
                 {"MARKA": false},
                 {"MODEL": false},
                 {"CENA": true},
@@ -21,13 +19,50 @@ router.post('/', function(req, res, next) {
                 {"MOC": true},
                 {"PRZEKĄTNA_EKRANU": true},
                 {"ROZDZIELCZOŚĆ": false}
+                //{"KARTA_GRAFICZNA": false},
+                //{"ILOŚĆ PAMIĘCI RAM": true},
+                //{"KATEGORIA_KATEGORIA_ID": false}
               ];
-
-            var sql = `INSERT INTO PRODUKT VALUES(`;
-            for (var i=1; i<cellsToAdd.length; i++)
+            
+            var chk = false;
+            var sqlString = `INSERT INTO PRODUKT(`;
+            for (var i=0; i<productFeatures.length; i++) 
             {
-                
+                if (data[key][i] === "") continue;
+                chk = true;
+                sqlString += `${Object.keys(productFeatures[i])[0]},`;
             }
+            if (chk == false) 
+            {
+                res.redirect('/');
+                return;
+            }
+            sqlString = sqlString.substring(0,sqlString.length-1);
+            sqlString += `) VALUES(`;
+            for (var i=0; i<productFeatures.length; i++) 
+            {
+                if (data[key][i] === "") continue;
+                if (productFeatures[i][Object.keys(productFeatures[i])[0]] == true) sqlString += `${data[key][i]},`;
+                else sqlString += `'${data[key][i]}',`;
+            }
+            sqlString = sqlString.substring(0,sqlString.length-1);
+            sqlString += `)`;
+            break; 
+        case 'produkt':
+            var productFeatures = [
+                {"MARKA": false},
+                {"MODEL": false},
+                {"CENA": true},
+                {"WAGA": true},
+                {"PROCESOR": false},
+                {"MOC": true},
+                {"PRZEKĄTNA_EKRANU": true},
+                {"ROZDZIELCZOŚĆ": false}
+                //{"KARTA_GRAFICZNA": false},
+                //{"ILOŚĆ PAMIĘCI RAM": true},
+                //{"KATEGORIA_KATEGORIA_ID": false}
+              ];
+              
             break;
         default:
             break;
